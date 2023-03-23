99d6f61ab30d0f007a7687c9c8092494a0c8ba9a
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-23 10:33:51 +0700

    Add project files.

---
+++ insertionsortojan/insertionsortojan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan Jumlah Data pada Array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout
+		}
+	}
+
+}
