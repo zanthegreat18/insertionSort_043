4c191598b7cbd9f60757a947e68c58fc10c1e310
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-23 10:44:49 +0700

    insertion sort ojan

--- insertionsortojan/insertionsortojan.cpp
+++ insertionsortojan/insertionsortojan.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]
-int n;
+int arr[20];						// Membuat Array dengan panjang data 20
+int n;								// Membuat Variable inputan n
 
-void input() {
+void input() {						//Procedure Input
 	while (true)
 	{
-		cout << "Masukan Jumlah Data pada Array : ";
-		cin >> n;
+		cout << "Masukan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+		cin >> n;									 // memanggil variable inputan n
 
-		if (n <= 20) {
+		if (n <= 20) {								 // membuat kondisi n tidak lebih dari 20
 			break;
 		}
 		else
 		{
-			cout << "\nArray yang anda masukan maksimal 20 Eleman.\n";
+			cout << "\n Array yang anda masukkan maksimal 20 elemen. \n"; // Menampilkan Pesan Jika  data lebih dari 20
 		}
 	}
+	cout << endl;									 // Membuat jarak perbaris program 
+	cout << "=====================" << endl;		 // Membuat tampilan susunan data elemen array
+	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;
 
+	for (int i = 0; i < n; i++)						 // Mengguanakan perualanga for untuk menyimpan data pada array
+	{
+
+	}
 }
